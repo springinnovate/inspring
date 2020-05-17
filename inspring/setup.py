6fe8358f2d7da92d32d1d0f377091e726b5eac55
Rich Sharp <richpsharp@gmail.com>
2020-05-17 15:49:54 -0700

    inital track

---
+++ inspring/setup.py
@@ -0,0 +1,61 @@
+"""setup.py module for inspring."""
+import numpy
+from setuptools.extension import Extension
+from setuptools import setup
+
+# Read in requirements.txt and populate the python readme with the non-comment
+# contents.
+_REQUIREMENTS = [
+    x for x in open('requirements.txt').read().split('\n')
+    if not x.startswith('#') and len(x) > 0]
+
+compiler_and_linker_args = ['-stdlib=libc++']
+
+setup(
+    name='inspring',
+    description="InSPRING: Computational Environment for experimental InVEST",
+    maintainer='Rich Sharp',
+    maintainer_email='richpsharp@gmail.com',
+    url='https://github.com/therealspring/inspring',
+    packages=[
+        'inspring',
+        'inspring.sdr_c_factor',
+    ],
+    package_dir={
+        'inspring': 'src/inspring'
+    },
+    use_scm_version={
+        'version_scheme': 'post-release',
+        'local_scheme': 'node-and-date'},
+    setup_requires=['setuptools_scm', 'cython', 'numpy'],
+    include_package_data=True,
+    install_requires=_REQUIREMENTS,
+    license='BSD',
+    zip_safe=False,
+    keywords='gis inspring',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Scientific/Engineering :: GIS',
+        'License :: OSI Approved :: BSD License'
+    ],
+    ext_modules=[
+        Extension(
+            name="inspring.sdr_c_factor.sdr_c_factor_core",
+            sources=["src/inspring/sdr/sdr_c_factor_core.pyx"],
+            include_dirs=[
+                numpy.get_include(),
+                'src/inspring/sdr'],
+            extra_compile_args=compiler_and_linker_args,
+            extra_link_args=compiler_and_linker_args,
+            language="c++",
+        )
+    ]
+)
