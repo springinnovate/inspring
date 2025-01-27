3a046193dc05f4142302b036efa57012ffb3e7bd
Richard P Sharp Jr <richpsharp@gmail.com>
2025-01-27 15:01:17 -0800

    try an astype

--- src/inspring/seasonal_water_yield/seasonal_water_yield.py
+++ src/inspring/seasonal_water_yield/seasonal_water_yield.py
@@ -830,12 +830,9 @@ def _calculate_monthly_quick_flow(
         # a_im is the mean rain depth on a rainy day at pixel i on month m
         # the 25.4 converts inches to mm since Si is in inches
         a_im = numpy.empty(valid_n_events.shape)
-        LOGGER.debug(f'valid_n_events: {valid_n_events.dtype}')
-        LOGGER.debug(f'p_i:m {p_im.dtype}')
-        sys.exit()
-        a_im = p_im[valid_mask] * (valid_n_events * 25.4)
+        a_im = p_im[valid_mask] / (valid_n_events.astype(float) * 25.4)
         qf_im = numpy.full(p_im.shape, qf_nodata)
-        #qf_im[valid_mask] = p_im
+        qf_im[valid_mask] = a_im
         return p_im
 
         # Precompute the last two terms in quickflow so we can handle a
@@ -930,7 +927,7 @@ def _calculate_monthly_quick_flow(
         # a_im is the mean rain depth on a rainy day at pixel i on month m
         # the 25.4 converts inches to mm since Si is in inches
         a_im = numpy.empty(valid_n_events.shape)
-        a_im = p_im[valid_mask] / (valid_n_events * 25.4)
+        a_im = p_im[valid_mask] / (valid_n_events.astype(float) * 25.4)
         qf_im = numpy.full(p_im.shape, qf_nodata)
 
         # Precompute the last two terms in quickflow so we can handle a
@@ -988,7 +985,7 @@ def _calculate_monthly_quick_flow(
     geoprocessing.raster_calculator(
         [(path, 1) for path in [
             precip_path, si_path, n_events_raster_path, stream_path]], qf_debug_op,
-        '%sp_im%s' % os.path.splitext(qf_monthly_path), gdal.GDT_Float32, qf_nodata)
+        '%sa_im%s' % os.path.splitext(qf_monthly_path), gdal.GDT_Float32, qf_nodata)
 
     geoprocessing.raster_calculator(
         [(path, 1) for path in [
